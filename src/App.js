7b6e183c170a91173575d7947243a0f36cc6a60d
agata <rybarska>
2022-09-12 17:36:15 +0200

    Generate random color & name it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { GetColorName } from 'hex-color-to-color-name';
+import { randomColor } from 'randomcolor';
+import { useState } from 'react';
 
 function App() {
+  const [hexCode, setHexCode] = useState('#ff0000');
+  const [colorName, setColorName] = useState('red');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Random Color Generator</h1>
+      <div
+        style={{
+          width: 400,
+          height: 400,
+          margin: '0 auto',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 30,
+          color: hexCode,
+        }}
+      >
+        Generated color: {colorName} {}
+        {hexCode}
+      </div>
+      <button
+        onClick={() => {
+          const newHexCode = randomColor();
+          setHexCode(newHexCode);
+          const newColorName = GetColorName(newHexCode);
+          setColorName(newColorName);
+        }}
+      >
+        Generate
+      </button>
     </div>
   );
 }
